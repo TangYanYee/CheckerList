2498859f4a8d9b601cfdc462923851f76dd8680e
AmberTang <72559487+TangYanYee@users.noreply.github.com>
2024-11-02 11:34:19 +0800

    website display

--- CheckerList/src/main.cpp
+++ CheckerList/src/main.cpp
@@ -1,69 +1,58 @@
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include <WiFi.h>
 
+#include "my_home_pw.h"
 // Replace with your network credentials
-const char* ssid = "ESP32-Access-Point";
-const char* password = "123456789";
-// Set web server port number to 80
-WiFiServer server(80);
+AsyncWebServer server(80);	// REPLACE WITH YOUR NETWORK CREDENTIALS
 
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";  // HTML web page to handle 3 input fields (input1, input2, input3)
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html><head>
+  <title>ESP Input Form</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head><body>
+  <form action="/get">
+    input1: <input type="text" name="input1"style="width: 50px">
+    |<input type="text" name="input2">
+    <input type="submit" value="Submit">
+  </form><br>
+</body></html>)rawliteral";
+void notFound(AsyncWebServerRequest* request) { request->send(404, "text/plain", "Not found"); }
 void setup() {
 	Serial.begin(115200);
-
-	// Connect to Wi-Fi network with SSID and password
-	Serial.print("Setting AP (Access Point)…");
-	// Remove the password parameter, if you want the AP (Access Point) to be open
-	WiFi.softAP(ssid, password);
-
-	IPAddress IP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(IP);
-
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(ssid, password);
+	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+		Serial.println("WiFi Failed!");
+		return;
+	}
+	Serial.println();
+	Serial.print("IP Address: ");
+	Serial.println(WiFi.localIP());
+	// Send web page with input fields to client
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest* request) { request->send_P(200, "text/html", index_html); });
+	// Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+	server.on("/get", HTTP_GET, [](AsyncWebServerRequest* request) {
+		String inputMessage;
+		String inputParam;
+		// GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+		if (request->hasParam(PARAM_INPUT_1)) {
+			inputMessage = request->getParam(PARAM_INPUT_1)->value();
+			inputParam = PARAM_INPUT_1;
+		}
+		Serial.println(inputMessage);
+		// GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+		if (request->hasParam(PARAM_INPUT_2)) {
+			inputMessage = request->getParam(PARAM_INPUT_2)->value();
+			inputParam = PARAM_INPUT_2;
+		}
+		Serial.println(inputMessage);
+		request->send_P(200, "text/html", index_html);
+	});
+	server.onNotFound(notFound);
 	server.begin();
 }
-
-void loop() {
-	WiFiClient client = server.available();	 // Listen for incoming clients
-
-	if (client) {						 // If a new client connects,
-		Serial.println("New Client.");	 // print a message out in the serial port
-		String currentLine = "";		 // make a String to hold incoming data from the client
-		while (client.connected()) {	 // loop while the client's connected
-			if (client.available()) {	 // if there's bytes to read from the client,
-				char c = client.read();	 // read a byte, then
-				Serial.write(c);		 // print it out the serial monitor
-				currentLine += c;
-				if (c == '\n') {  // if the byte is a newline character
-					// if the current line is blank, you got two newline characters in a row.
-					// that's the end of the client HTTP request, so send a response:
-					if (currentLine.length() == 2) {
-						// HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-						// and a content-type so the client knows what's coming, then a blank line:
-						client.println("HTTP/1.1 200 OK");
-						client.println("Content-type:text/html");
-						client.println("Connection: close");
-						client.println();
-
-						// Send your "Hello World" HTML response
-						client.println(
-							"<html><head><meta name=\"viewport\" content=\"width=device-width, "
-							"initial-scale=1\"></head>");
-						client.println("<body><h1>Hello World</h1></body></html>");
-
-						// The HTTP response ends with another blank line
-						client.println();
-						// Break out of the while loop
-						break;
-					} else {  // if you got a newline, then clear currentLine
-						currentLine = "";
-					}
-				} else if (c != '\r') {	 // if you got anything else but a carriage return character,
-					currentLine += c;	 // add it to the end of the currentLine
-				}
-			}
-		}
-		// Close the connection
-		client.stop();
-		Serial.println("Client disconnected.");
-		Serial.println("");
-	}
-}
+void loop() {}
